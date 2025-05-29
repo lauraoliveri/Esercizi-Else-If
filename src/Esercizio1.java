4e0ac41cc07a51ce04430fc4e283a94ab4e066e5
lauraoliveri <lauraoliveri2003@gmail.com>
2025-05-29 19:04:10 +0200

    Esercizio 1 completato

---
+++ src/Esercizio1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Esercizio1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Inserisci un voto da 0 a 100");
+
+        int voto = scan.nextInt();
+
+        if (voto >= 90) {
+            if (voto < 100)
+                System.out.println("La tua valutazione: Ottimo");
+            else  if (voto > 100)
+                System.out.println("Inserisci un voto valido");
+
+        }else if (voto >=80) {
+            System.out.println("La tua valutazione: Buono");
+        }else if (voto >= 70) {
+            System.out.println("La tua valutazione: Sufficiente");
+        }else if (voto >=60) {
+            System.out.println("La tua valutazione: Rimandato");
+        }else if (voto < 60) {
+            System.out.println("La tua valutazione: Bocciato");
+        }
+    }
+}
